0a90841ed3f71e0a4cd1f3887f50526df27394b7
Jinu Lee <aquamrn@snu.ac.kr>
2022-02-19 18:44:01 +0900

    added new loss function answer_in_context

--- kogpt2/loss.py
+++ kogpt2/loss.py
@@ -33,8 +33,9 @@ def question_gen_loss(inputs, logits, questions, answers):
   Only retrieve QAG part generation loss, excluding the context
   """
   start_index = questions[0] + 1
-  end_index = answers[-1] + 1
-  inputs = inputs[start_index:end_index]
+  end_index = answers[-1]
+  # shift inputs
+  inputs = inputs[start_index+1:end_index+1]
   logits = logits[start_index:end_index]
   return F.cross_entropy(logits, inputs)
   
@@ -42,7 +43,22 @@ def question_gen_loss(inputs, logits, questions, answers):
 def answer_in_context_loss(inputs, logits, questions, answers):
   """
   Penalize gpt model when it does not generate answer from the context.
+  Instead of strict copy generation, we softly train the model to find the answer from the context.
   """
+  body_tokens = list(set(inputs[1:questions[0]])) + [QUES_TOKEN, SEP_TOKEN]
+
+  # Set mask
+  mask = torch.ones(logits.size(1)).to(logits.device)
+  mask[body_tokens] = 0
+  mask.requires_grad = False
+
+  answer_logits = torch.cat([logits[a+1:q] for q, a in zip(answers, questions[1:])], dim=0)
+  answer_logits = F.softmax(answer_logits, dim=1)
+
+  loss = torch.sum(answer_logits * mask.unsqueeze(0))
+
+  return loss
+
 
 def sep_token_loss(inputs, logits, questions, answers):
   """
@@ -55,15 +71,15 @@ def question_diversity(inputs, logits, questions, answers):
   """
 
 
-def lossQAG(inputs, logits, loss_list):
+def lossQAG(input_batch, logit_batch, loss_dict):
 
   loss = 0
-  for i in range(inputs.size(0)):
-    input = inputs[i]
-    logit = logits[i]
+  for i in range(input_batch.size(0)):
+    inputs = input_batch[i]
+    logits = logit_batch[i]
 
-    questions, answers = parse_index(input)
+    questions, answers = parse_index(input_batch[i])
 
-    for loss_fn in loss_list:
-      loss += globals()[loss_fn](input, logit, questions, answers) # FIXME: globals() may not be secure method to call by name
+    for loss_fn in loss_dict:
+      loss += globals()[loss_fn](inputs, logits, questions, answers) * loss_dict[loss_fn] # FIXME: globals() may not be secure method to call by name
   return loss
